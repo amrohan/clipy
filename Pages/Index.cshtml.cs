aabd6648bbc4ffd9f9e15157ff1afceeb77faf96
amrohan <rohanxsalunkhe@gmail.com>
2026-04-12 20:45:27 +0530

    feat: add auth flow for login

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,19 +3,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace clipy.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel(ILogger<IndexModel> logger) : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<IndexModel> _logger = logger;
 
     public int CurrentYear { get; private set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IActionResult OnGet()
     {
-        _logger = logger;
-    }
+        if (User.Identity is { IsAuthenticated: true })
+        {
+            return RedirectToPage("/MyNotes");
+        }
 
-    public void OnGet()
-    {
         CurrentYear = DateTime.Now.Year;
+
+        return Page();
     }
 }
